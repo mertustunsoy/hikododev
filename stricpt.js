c51eda14cdca418c643bedab8ebb196411e58977
mertustunsoy <144946520+mertustunsoy@users.noreply.github.com>
2023-10-26 22:34:25 +0300

    Add files via upload

---
+++ stricpt.js
@@ -0,0 +1,7 @@
+let sayi = 10;
+
+if (sayi > 5) {
+  console.log("Sayı 5'ten büyüktür");
+} else {
+  console.log("Sayı 5'ten küçük veya eşittir");
+}
---
+++ stricpt1.js
@@ -0,0 +1,12 @@
+let sayi1 = 10;
+let sayi2 = 20;
+
+let toplam = sayi1 + sayi2;
+let fark = sayi1 - sayi2;
+let carpma = sayi1 * sayi2;
+let bolme = sayi1 / sayi2;
+
+console.log("Toplam: " + toplam);
+console.log("Fark: " + fark);
+console.log("Çarpma: " + carpma);
+console.log("Bölme: " + bolme);
